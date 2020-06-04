97ff104ff1cbd1e7e492657110b4cafcdead6744
Bogdan2002g <62776475+Bogdan2002g@users.noreply.github.com>
2020-06-04 22:44:26 +0300

    1

---
+++ лабораторна 2f/AboutUs.php
@@ -0,0 +1,88 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <title>AboutUS</title>
+    <link href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body{
+            background-image: url("CSS/img/lol1.jpg");
+
+
+        }
+
+
+        h1{
+            color: #FFA812;
+            text-shadow: black 2px 3px 1px;
+            text-align: center;
+        }
+
+
+
+        p{
+
+            font-size: 30px;
+            font-family: Tahoma;
+        }
+
+
+
+
+
+    </style>
+</head>
+
+
+<div class="container bgcont center-block">
+    <div class="col-md-12">
+<header style="margin-bottom: 20px">
+
+    <div>
+        <a class="btn btn-dark" href="index.php" role="button"> ← Повернутися на головну сторіку </a>
+    </div>
+
+</header>
+
+
+        <body   >
+
+
+
+        <div class="col-md-12">
+            <p>Цей веб-сайт був спецілально створенй для демонстрації свохїх навичок
+                з дисципліни веб-технології студентом групи КІ2-19-1.</p>
+
+            <p>Зі мною можливо зв’язатися за електронною адресою: bogdangutsal2002@gmail.com</p>
+
+            <p>або в соцмережах</p>
+
+            <a href="https://www.instagram.com/bogdan_gutsal_/">
+
+                <img  class="photo" src= "images/photo1.jpg" alt= "фото" width="40px"  >
+            </a>
+
+
+            <a href="https://www.facebook.com/profile.php?id=100016536528360">
+
+                <img  class="photo"  src= "images/photo2.png" alt= "фото" width="40px" >
+
+            </a>
+
+            <a href="https://vk.com/bogdan2002g">
+                <img  class="photo"  src= "images/photo3.jpg" alt= "фото" width="40px"  >
+            </a>
+
+
+
+
+
+        </div>
+
+
+        </body>
+</html>
